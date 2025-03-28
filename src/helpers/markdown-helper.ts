438511fc9bc03deb6e3ff85b5410ad91bf819840
HyunHo Lee <dlgusgh2001@naver.com>
2025-03-28 11:13:50 +0900

    refactor: 마크다운 헬퍼 - 순수 함수 구성

--- src/helpers/markdown-helper.ts
+++ src/helpers/markdown-helper.ts
@@ -4,25 +4,25 @@ import { remark } from "remark";
 import html from "remark-html";
 
 // Velog 백업 파일이 있는 디렉터리
-function getBlogDirectory(): string {
-  return path.join(process.cwd(), "src", "backup", "content");
-}
+const getBlogDirectory = (): string =>
+  path.join(process.cwd(), "src", "backup", "content");
 
-function getBlogPostPullNameList(): string[] {
-  return fs.readdirSync(getBlogDirectory());
-}
+const getBlogPostPullNameList = (): string[] =>
+  fs.readdirSync(getBlogDirectory());
 
-export function getBlogPostNameList(): string[] {
-  return getBlogPostPullNameList().map((fileName: string): string => {
-    return fileName.replace(/\.md$/, "");
-  });
-}
+const removeMarkdownExtension = (fileName: string): string =>
+  fileName.replace(/\.md$/, "");
+
+export const getBlogPostNameList = (): string[] => {
+  const fileNameList = getBlogPostPullNameList();
+  return fileNameList.map(removeMarkdownExtension);
+};
 
 // 마크다운 파일을 읽어서 HTML로 변환
-export async function getMarkdownContent(fileName: string): Promise<string> {
+export const getMarkdownContent = async (fileName: string): Promise<string> => {
   const filePath = path.join(getBlogDirectory(), `${fileName}.md`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const processedContent = await remark().use(html).process(fileContent);
 
   return processedContent.toString();
-}
+};
