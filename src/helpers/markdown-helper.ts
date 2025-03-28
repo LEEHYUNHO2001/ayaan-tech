d39c8bc0464dfd125656606a993f6b3584545311
HyunHo Lee <dlgusgh2001@naver.com>
2025-03-28 10:52:04 +0900

    feat: 마크다운 헬퍼 - 파일 리스트 담긴 배열을 리턴하는 함수 생성

--- src/helpers/markdown-helper.ts
+++ src/helpers/markdown-helper.ts
@@ -3,17 +3,26 @@ import path from "path";
 import { remark } from "remark";
 import html from "remark-html";
 
+// Velog 백업 파일이 있는 디렉터리
+function getBlogDirectory(): string {
+  return path.join(process.cwd(), "src", "backup", "content");
+}
+
+function getBlogPostPullNameList(): string[] {
+  return fs.readdirSync(getBlogDirectory());
+}
+
+export function getBlogPostNameList(): string[] {
+  return getBlogPostPullNameList().map((fileName: string): string => {
+    return fileName.replace(/\.md$/, "");
+  });
+}
+
 // 마크다운 파일을 읽어서 HTML로 변환
 export async function getMarkdownContent(fileName: string): Promise<string> {
-  const filePath = path.join(
-    process.cwd(),
-    "src",
-    "backup",
-    "content",
-    `${fileName}.md`
-  );
+  const filePath = path.join(getBlogDirectory(), `${fileName}.md`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
-
   const processedContent = await remark().use(html).process(fileContent);
+
   return processedContent.toString();
 }
