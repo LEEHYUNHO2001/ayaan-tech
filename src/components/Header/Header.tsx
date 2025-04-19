fe069a78ecc684ea80f197f0d022ad5f01a1cfa4
HyunHo Lee <dlgusgh2001@naver.com>
2025-04-19 13:43:32 +0900

    fix: Header 색상 조정
    - 라이트 모드 대응

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import { useRouter, usePathname } from "next/navigation";
 import Tag from "@/components/Tag/Tag";
 import { HEADER_MENU_LIST, HeaderMenu } from "@/constants/header";
 import { FC, JSX } from "react";
+import ThemeToggleButton from "@/components/ThemeToggleButton/ThemeToggleButton";
 
 const Header: FC = (): JSX.Element => {
   const router = useRouter();
@@ -26,12 +27,14 @@ const Header: FC = (): JSX.Element => {
   };
 
   return (
-    <header className="fixed left-0 right-0 top-0 border-b border-grey bg-[rgba(45,47,57,0.8)] bg-opacity-5 backdrop-blur-sm">
+    <header className="fixed left-0 right-0 top-0 border-b border-primary bg-[rgba(225,245,255,0.8)]	bg-opacity-5 backdrop-blur-sm dark:border-grey dark:bg-[rgba(45,47,57,0.8)]">
       <div className="container flex cursor-pointer items-center justify-between px-6 py-4">
         <div className="flex items-center gap-2" onClick={handleOnClickHome}>
           Logo
           <h1 className="text-xl font-bold">ayaan tech</h1>
         </div>
+
+        <ThemeToggleButton />
 
         <ul className="flex items-center gap-2">
           {HEADER_MENU_LIST.map((headerMenu: HeaderMenu) => (
