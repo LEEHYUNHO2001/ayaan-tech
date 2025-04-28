2ed2531e7967a9e89852f7abe87eb8760d5ca6a3
HyunHo Lee <dlgusgh2001@naver.com>
2025-04-28 22:40:56 +0900

    feat: 커리어 페이지 type 파일 생성

---
+++ src/app/careers/careers.type.ts
@@ -0,0 +1,17 @@
+interface Education {
+  label: string;
+  description: string;
+  period: string[];
+}
+
+export interface Career extends Education {
+  mainRole: string;
+  subRole?: string;
+  stackList: string[];
+}
+
+export interface CareersHomeTextModel {
+  title: string;
+  careerList: Career[];
+  educationList: Education[];
+}
