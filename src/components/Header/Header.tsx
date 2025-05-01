0986a558974a5dfecb3135fd57e05f4e369ab195
HyunHo Lee <dlgusgh2001@naver.com>
2025-05-01 23:18:48 +0900

    fix: Header 컴포넌트 z-index 추가
    - Career Card가 아래로 가게 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,7 +27,7 @@ const Header: FC = (): JSX.Element => {
   };
 
   return (
-    <header className="fixed left-0 right-0 top-0 border-b border-primary bg-[rgba(225,245,255,0.8)]	bg-opacity-5 backdrop-blur-sm dark:border-grey dark:bg-[rgba(45,47,57,0.8)]">
+    <header className="fixed left-0 right-0 top-0 z-50 border-b border-primary bg-[rgba(225,245,255,0.8)]	bg-opacity-5 backdrop-blur-sm dark:border-grey dark:bg-[rgba(45,47,57,0.8)]">
       <div className="container flex cursor-pointer items-center justify-between px-6 py-4">
         <div className="flex items-center gap-2" onClick={handleOnClickHome}>
           Logo
