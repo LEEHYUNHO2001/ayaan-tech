f5c52135e4c066f3ecc2d88c92c9514e11ca9baa
HyunHo Lee <dlgusgh2001@naver.com>
2025-05-20 21:50:32 +0900

    fix: postCard 컴포넌트 hide 옵셔 추가

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -7,9 +7,15 @@ import Tag from "@/components/Tag/Tag";
 
 interface PostCardProps {
   post: PostCardModel;
+  hideTag?: boolean;
+  hideDate?: boolean;
 }
 
-const PostCard: FC<PostCardProps> = ({ post }): JSX.Element => {
+const PostCard: FC<PostCardProps> = ({
+  post,
+  hideTag,
+  hideDate,
+}): JSX.Element => {
   const router = useRouter();
 
   const handleOnClickPost = (): void => {
@@ -21,22 +27,26 @@ const PostCard: FC<PostCardProps> = ({ post }): JSX.Element => {
       className="dark: flex cursor-pointer items-start justify-between rounded-2xl border border-primary bg-primaryLight px-6 py-4 dark:border-greyLight dark:bg-grey"
       onClick={handleOnClickPost}
     >
-      <div className="flex w-3/4 flex-col gap-2">
+      <div className={`flex flex-col gap-2 ${hideDate ? "w-full" : "w-3/4"}`}>
         <span className="text-xl font-bold">{post.name}</span>
         <p className="text-sm text-secondary dark:text-greyLight">
           {post.description}
         </p>
-        <ul className="flex gap-2">
-          {post.tagList.map((tag) => (
-            <li key={tag}>
-              <Tag label={tag} bg={"dark:bg-greyDark bg-primary"} />
-            </li>
-          ))}
-        </ul>
+        {!hideTag && (
+          <ul className="flex gap-2">
+            {post.tagList.map((tag) => (
+              <li key={tag}>
+                <Tag label={tag} bg={"dark:bg-greyDark bg-primary"} />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-      <span className="text-sm text-secondary dark:text-greyLight">
-        {post.date}
-      </span>
+      {!hideDate && (
+        <span className="text-sm text-secondary dark:text-greyLight">
+          {post.date}
+        </span>
+      )}
     </div>
   );
 };
