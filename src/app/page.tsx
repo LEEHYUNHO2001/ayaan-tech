7f1400bc90392375c3c9e8327245e6f9291044c1
HyunHo Lee <dlgusgh2001@naver.com>
2025-05-27 21:48:12 +0900

    fix: 홈 페이지 재구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,58 @@
+import dayjs from "dayjs";
 import {
   BlogPostMeta,
-  getBlogBackupSpecificPostList,
-  transformPostList,
+  getBlogBackupPostList,
 } from "@/helpers/backup-markdown-list-helper";
-import PostCard from "@/components/PostCard/PostCard";
-import { PostCardModel } from "@/components/PostCard/postCard.type";
-import { memoirFileNameList, recommendFileNameList } from "@/constants/post";
-import dayjs from "dayjs";
+import BlogChart from "@/components/BlogChart/BlogChart";
+import HomePostSection from "@/components/HomePostSection/HomePostSection";
 
 export default function HomePage() {
   const EXPERIENCE_START_DATE = "2022-04-01";
-  const blogMemoirPostList: BlogPostMeta[] =
-    getBlogBackupSpecificPostList(memoirFileNameList);
-  const blogRecommendPostList: BlogPostMeta[] = getBlogBackupSpecificPostList(
-    recommendFileNameList
+  const blogBackupPostList: BlogPostMeta[] = getBlogBackupPostList();
+  const blogPostDateList: string[] = blogBackupPostList.map(
+    (blogBackupPost: BlogPostMeta) => blogBackupPost.dayjs.format("YYYY-MM")
   );
-  const transformedMemoirPostList = transformPostList(blogMemoirPostList);
-  const transformedRecommendPostList = transformPostList(blogRecommendPostList);
+  const blogBackupPostLength = blogBackupPostList.length;
 
   const getDiffYear = (): number => dayjs().diff(EXPERIENCE_START_DATE, "M");
   const getExperienceYear = (): number => Math.round(getDiffYear() / 12);
   const blogHomeTextModel = {
     title: "소개",
-    description: `
-      안녕하세요, 프론트엔드 개발자 아얀입니다. 경력은 어느덧 ${getExperienceYear()}년 차가 되었네요.
-      저는 평소에 중요하다고 느끼는 내용들을 정리하며 기록하는 습관이 있습니다. 이 기록들을 혼자만 보기 아깝다는 생각에, 블로그를 시작하게 되었습니다.
-      개발자가 되기 전부터 기술 블로그를 작성해왔으며, 현재는 실무에서의 경험을 바탕으로 실제로 고민했던 문제들과 그 해결 과정을 중심으로 글을 쓰고 있습니다.
+    description: `안녕하세요, 프론트엔드 개발자 아얀입니다.
+      어느덧 경력 ${getExperienceYear()}년 차가 되었네요.
+      
+      저는 평소 중요하다고 느낀 내용을 정리하고 기록하는 습관이 있습니다. 이 기록들이 단지 저만 보기엔 아깝다는 생각이 들어 블로그를 시작하게 되었습니다.
+      
+      개발자가 되기 전부터 기술 블로그를 작성해왔기 때문에, 예전 글을 보면 당연한 내용을 적어두거나 잘못된 정보도 있을 수 있습니다. 하지만 그 모든 기록이 저의 성장 과정이라 생각하기에 소중히 간직하고 있습니다.
+      
+      현재는 실무에서 실제로 겪은 문제들과 그에 대한 고민, 해결 과정을 중심으로 조금 더 깊이 있는 글을 쓰고자 노력하고 있습니다.
     `,
   };
 
   return (
     <section className="py-10">
-      <h1 className="mb-5 text-4xl font-bold">{blogHomeTextModel.title}</h1>
+      <article className="mb-10 flex gap-6 border-b pb-5">
+        <div className="flex flex-1 flex-col">
+          <h1 className="mb-5 text-3xl font-bold">{blogHomeTextModel.title}</h1>
 
-      <div className="mb-10 flex flex-col border-b pb-5">
-        <div className="flex">
-          <ul className="flex-1 text-lg leading-7">
-            <li className="flex gap-2">
-              <span>이메일 :</span>
-              <a href="mailto:dlgusgh200240@gmail.com">
-                dlgusgh200240@gmail.com
-              </a>
-            </li>
-
-            <li className="flex gap-2">
-              <span>기술 블로그 :</span>
-              <a
-                href="https://velog.io/@leehyunho2001/posts"
-                target="_blank"
-                className="hover:underline"
-              >
-                Velog
-              </a>
-            </li>
-
-            <li className="flex gap-2">
-              <span>깃 허브 :</span>
-              <a
-                href="https://github.com/LEEHYUNHO2001"
-                target="_blank"
-                className="hover:underline"
-              >
-                LEEHYUNHO2001
-              </a>
-            </li>
-          </ul>
-
-          <p className="flex-1 whitespace-pre-line text-lg leading-7">
-            {/*{blogHomeTextModel.description}*/}
-          </p>
-        </div>
-
-        <p className="pt-10 text-lg">{blogHomeTextModel.description}</p>
-      </div>
-
-      <article className="flex gap-4">
-        <div className="flex-1">
-          <h2 className="mb-4 text-lg font-bold">회고글</h2>
-          <ul className="flex flex-col gap-2.5">
-            {transformedMemoirPostList.map((post: PostCardModel) => (
-              <li key={post.name}>
-                <PostCard post={post} hideTag={true} hideDate={true} />
-              </li>
-            ))}
-          </ul>
+          <p className="whitespace-pre-line">{blogHomeTextModel.description}</p>
         </div>
 
         <div className="flex-1">
-          <h2 className="mb-4 text-lg font-bold">추천글</h2>
-          <ul className="flex flex-col gap-2.5">
-            {transformedRecommendPostList.map((post: PostCardModel) => (
-              <li key={post.name}>
-                <PostCard post={post} hideTag={true} hideDate={true} />
-              </li>
-            ))}
-          </ul>
+          <div className="mb-8 pl-8">
+            <h3 className="mb-2 text-xl font-bold">요약</h3>
+
+            <ul className="text-sm text-gray-600 dark:text-gray-300">
+              <li>📚 총 포스트 수: {blogBackupPostLength}개</li>
+              <li>📝 주요 주제: 프론트엔드, 실무 회고, 커리어</li>
+              <li>🎯 목표: 경험 공유를 통한 성장</li>
+            </ul>
+          </div>
+          <BlogChart postDateList={blogPostDateList} />
         </div>
       </article>
+
+      <HomePostSection />
     </section>
   );
 }
