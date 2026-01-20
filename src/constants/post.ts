b56184e3ce3f014a31b3a5bcbd327e8bdf7ac64a
ayaan <dlgusgh2001@naver.com>
2026-01-20 18:35:19 +0900

    feat: 메인 페이지 추천 글 수정

--- src/constants/post.ts
+++ src/constants/post.ts
@@ -1,4 +1,5 @@
 export const memoirFileNameList: string[] = [
+  "눈떠보니 4년차의 회고글.md",
   "평범한 3년차 개발자의 회고글.md",
   "평범한 주니어 개발자의 회고글.md",
   "평범한 개발자의 늦은 회고글.md",
@@ -8,4 +9,5 @@ export const recommendFileNameList: string[] = [
   "나도 드디어 Next.js 쓰는건가!.md",
   "주도적으로 설계하는 MFA.md",
   "디자인 시스템 장점 극대화하기.md",
+  "디자인 토큰으로 설계하는 디자인 시스템.md",
 ];
